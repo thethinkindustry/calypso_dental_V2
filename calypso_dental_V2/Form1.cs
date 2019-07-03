14072553394f08963890de2ce6c09be93da52058
mmmerttuncel <41569309+mmmerttuncel@users.noreply.github.com>
2019-07-03 13:35:34 +0300

    init commit

---
+++ calypso_dental_V2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using MaterialSkin.Controls; 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calypso_dental_V2
+{
+    public partial class frm_main :MaterialForm 
+    {
+        private Settings ayarlar = new Settings();
+        public frm_main()
+        {
+            InitializeComponent();
+            //  ayarlar.ServerIP = "192.168.1.15";
+
+            var teet = grb_teeth.Controls.OfType<PictureBox>();
+            foreach (PictureBox te in teet)
+            {
+                te.MouseClick += new MouseEventHandler((o, a) => onClickList(te));
+            }
+           
+           
+        }
+
+        public void onClickList(PictureBox te)
+        {
+            if (te.BackColor == Color.DarkSlateGray)
+            {
+                te.BackColor = Color.Transparent;
+            }
+            else
+            {
+                te.BackColor = Color.DarkSlateGray;
+            }
+
+        }     
+    }
+   
+    }
+
---
+++ calypso_dental_V2/patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calypso_dental_V2
+{
+   public class patient
+    {
+        public string drName { get; set; }
+        public string pName { get; set; }
+        public string progName { get; set; }
+        public string teet { get; set; }
+        public string color { get; set; }
+        public int uprice{ get; set; }
+        public int price { get; set; }
+        public DateTime regDate { get; set; }
+        public DateTime delDate { get; set; }
+    }
+}
